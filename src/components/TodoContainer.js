df64828ea9d870aceb81e32f9c7d942e1d581d1b
clintonjosephs <clintonjosephs26@gmail.com>
2022-02-13 23:10:00 +0100

    set up all component files for todo-app

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/prefer-stateless-function */
+
+import React, { Component } from 'react';
+
+class TodoContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Create React App</h1>
+        <p>I am in a react Component</p>
+      </div>
+    );
+  }
+}
+
+export default TodoContainer;
