07c41336d1ba8aaa823831bf16716b416f31da07
clintonjosephs <clintonjosephs26@gmail.com>
2022-02-13 23:32:32 +0100

    used arraymap to add list items

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,11 +3,39 @@
 import React, { Component } from 'react';
 
 class TodoContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    };
+  }
+
   render() {
+    const { todos } = this.state;
+
     return (
       <>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a react Component</p>
+        <ul>
+          {todos.map(({ id, title }) => (
+            <li key={id}>{title}</li>
+          ))}
+        </ul>
       </>
     );
   }
