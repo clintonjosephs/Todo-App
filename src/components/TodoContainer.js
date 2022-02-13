33833a38414fd692e78b385ff67b96edabaadcaa
clintonjosephs <clintonjosephs26@gmail.com>
2022-02-13 23:14:29 +0100

    adding strict mode to help show warnings for quick fixs

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -5,10 +5,10 @@ import React, { Component } from 'react';
 class TodoContainer extends Component {
   render() {
     return (
-      <div>
+      <>
         <h1>Hello from Create React App</h1>
         <p>I am in a react Component</p>
-      </div>
+      </>
     );
   }
 }
