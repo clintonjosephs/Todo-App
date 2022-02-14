de815930b086f7f4173944fd5d8e253307eb1603
clintonjosephs <clintonjosephs26@gmail.com>
2022-02-14 11:36:53 +0100

    added error message dynamically, remove after 2 seconds on successful submit

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -2,15 +2,15 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 const ErrorMessage = (props) => {
-    const {display, message} = props;
-    return (
-        <div style={display}>{message}</div>
-    );
-}
+  const { display, message } = props;
+  return (
+    <div style={{ display }}>{message}</div>
+  );
+};
 
 ErrorMessage.propTypes = {
-    display: propTypes.string.isRequired,
-    message: propTypes.strring.isRequired
-}
+  display: propTypes.string.isRequired,
+  message: propTypes.string.isRequired,
+};
 
 export default ErrorMessage;
--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import ErrorMessage from './ErrorMessage';
 
 class InputTodo extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: '',
-      errorDisplay: false,
+      errorDisplay: 'none',
+      message: '',
     };
   }
 
   titleChangeHandler = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({
+      [event.target.name]: event.target.value,
+      errorDisplay: 'none',
+    });
   };
 
   todoSubmitHandler = (event) => {
@@ -19,31 +24,44 @@ class InputTodo extends Component {
     const { title } = this.state;
     if (title.trim()) {
       const { addTodoItemHandler } = this.props;
+
       addTodoItemHandler(title);
+
       this.setState({
         title: '',
-        errorDisplay: false,
+        errorDisplay: 'block',
+        message: 'Item added to list successfully',
       });
+
+      setTimeout(() => {
+        this.setState({
+          errorDisplay: 'none',
+        });
+      }, 2000);
     } else {
       this.setState({
-        errorDisplay: true,
+        errorDisplay: 'block',
+        message: 'Please, type-in a todo item',
       });
     }
   };
 
   render() {
-    const { title } = this.state;
+    const { title, errorDisplay, message } = this.state;
     return (
-      <form onSubmit={this.todoSubmitHandler}>
-        <input
-          type="text"
-          name="title"
-          placeholder="Add Todo ..."
-          value={title}
-          onChange={this.titleChangeHandler}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <>
+        <ErrorMessage display={errorDisplay} message={message} />
+        <form onSubmit={this.todoSubmitHandler}>
+          <input
+            type="text"
+            name="title"
+            placeholder="Add Todo ..."
+            value={title}
+            onChange={this.titleChangeHandler}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </>
     );
   }
 }
